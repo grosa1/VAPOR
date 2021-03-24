1c1bc5afa00dee698646eedb2cbbe588f71ed865
Scott Pearse <pearse@ucar.edu>
2021-03-24 10:33:10 -0600

    bump from 32 to 64

--- .circleci/downloadWin3rdParty.py
+++ .circleci/downloadWin3rdParty.py
@@ -1,8 +1,9 @@
 import gdown 
 url = "https://drive.google.com/a/ucar.edu/uc?id=1fzZ-mbY4Cek1TRsaKm79a08Od5gNCogk"
-output = "2019-Aug-Win32.zip"
+#output = "2019-Aug-Win32.zip"
+output = "2019-Aug-Win64.zip"
 gdown.download(url, output, quiet=False)
 
 import zipfile
-with zipfile.ZipFile("2019-Aug-Win32.zip", 'r') as zip_ref:
+with zipfile.ZipFile("2019-Aug-Win64.zip", 'r') as zip_ref:
     zip_ref.extractall('C:\\')
