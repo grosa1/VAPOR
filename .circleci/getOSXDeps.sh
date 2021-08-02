ff98667c80dc6e3baa85961a07b039f20da151d9
Scott Pearse <pearse@ucar.edu>
2021-08-02 10:25:41 -0600

    test

--- .circleci/getOSXDeps.sh
+++ .circleci/getOSXDeps.sh
@@ -20,4 +20,4 @@ cd MacPorts-2.7.1/
 make
 sudo make install
 sudo /opt/local/bin/port selfupdate
-sudo yes | sudo /opt/local/bin/port install llvm-12 -N
+sudo (yes || true) | sudo /opt/local/bin/port install llvm-12
