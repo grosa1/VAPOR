7dc9dfa19d2d2cc494ddf686cd172531c4623731
sgpearse <pearse@ucar.edu>
2023-04-05 13:58:48 -0600

    test

--- scripts/build3rdParty.sh
+++ scripts/build3rdParty.sh
@@ -39,19 +39,26 @@ osxPrerequisites() {
 }
 
 ubuntuPrerequisites() {
-    apt update
-    apt upgrade
+    apt update -y
+    apt upgrade -y
 
     # All of this for updating to cmake > 3.16
-    apt remove --purge --auto-remove cmake
-    apt install -y software-properties-common lsb-release && \
-    apt clean all
-    wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-    apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
-    apt install kitware-archive-keyring
-    rm /etc/apt/trusted.gpg.d/kitware.gpg
-    apt update
-    apt install cmake
+    #apt remove --purge --auto-remove cmake
+    #apt install -y software-properties-common lsb-release && \
+    #apt clean all
+    #wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+    #apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+    #apt install kitware-archive-keyring
+    #rm /etc/apt/trusted.gpg.d/kitware.gpg
+    #apt update
+    #apt install cmake
+    apt-get update
+    apt-get install -y gpg wget
+    wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | tee /usr/share/keyrings/kitware-archive-keyring.gpg >/dev/null
+    echo 'deb [signed-by=/usr/share/keyrings/kitware-archive-keyring.gpg] https://apt.kitware.com/ubuntu/ focal main' | tee /etc/apt/sources.list.d/kitware.list >/dev/null
+    DEBIAN_FRONTEND=noninteractive apt install -y software-properties-common
+    apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main'
+    apt install -y cmake --allow-unauthenticated
 
     apt install -y m4 libcurl4-openssl-dev libxau-dev autoconf libtool libxcb-xinerama0
 
