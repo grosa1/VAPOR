1a78a8efee619e695250feab647511aacce285c9
sgpearse <pearse@ucar.edu>
2023-04-06 13:57:31 -0600

    test

--- scripts/build3rdParty.sh
+++ scripts/build3rdParty.sh
@@ -59,7 +59,7 @@ ubuntuPrerequisites() {
 
 centosPrerequisites() {
 	yum update -y
-	yum install -y epel-release kernel-devel gcc cmake3 make xz-devel zlib-devel openssl-devel
+	yum install -y epel-release kernel-devel gcc cmake3 make xz-devel zlib-devel openssl-devel gcc-c++
     shopt -s expand_aliases
     echo alias cmake=\'cmake3\' >> ~/.bashrc
     . ~/.bashrc
