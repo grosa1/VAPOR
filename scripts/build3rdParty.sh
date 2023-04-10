018e81513e962a1182809281f980e512acb1d74d
sgpearse <pearse@ucar.edu>
2023-04-10 14:11:17 -0600

    test

--- scripts/build3rdParty.sh
+++ scripts/build3rdParty.sh
@@ -441,7 +441,7 @@ qt() {
         #local library='qt-everywhere-src-5.13.2'
         yum install -y wget
         wget https://download.qt.io/archive/qt/5.13/5.13.2/single/qt-everywhere-src-5.13.2.tar.xz
-        tar xvf $baseDir/$library.tar.xz && cd $baseDir/$library
+        tar xvf $baseDir/$library.tar.xz.1 && cd $baseDir/$library
     else 
         local library='qt-everywhere-opensource-src-5.15.8'
         tar xvf $baseDir/$library.tar.xz && cd $baseDir/qt-everywhere-src-5.15.8
