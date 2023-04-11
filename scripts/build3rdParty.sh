b5a8ba406d1064b02b45da3db5ca00be522ed248
sgpearse <pearse@ucar.edu>
2023-04-11 08:58:59 -0600

    test

--- scripts/build3rdParty.sh
+++ scripts/build3rdParty.sh
@@ -432,21 +432,23 @@ qt() {
     cd $baseDir
     if [ $OS == "CentOS" ]; then
         local library='qt-everywhere-src-5.13.2'
-        yum install -y wget
-        rm -rf /usr/local/VAPOR-Deps/2023-Mar-src/qt-everywhere-src-5.13.2.tar.xz
-        wget https://download.qt.io/archive/qt/5.13/5.13.2/single/qt-everywhere-src-5.13.2.tar.xz
-        tar xvf $baseDir/$library.tar.xz && mkdir -p $baseDir/$library/build && cd !$
+        #yum install -y wget
+        #rm -rf /usr/local/VAPOR-Deps/2023-Mar-src/qt-everywhere-src-5.13.2.tar.xz
+        #wget https://download.qt.io/archive/qt/5.13/5.13.2/single/qt-everywhere-src-5.13.2.tar.xz
+        #tar xvf $baseDir/$library.tar.xz && mkdir -p $baseDir/$library/build && cd !$
     else 
         local library='qt-everywhere-opensource-src-5.15.8'
-        tar xvf $baseDir/$library.tar.xz && cd $baseDir/qt-everywhere-src-5.15.8
+        #tar xvf $baseDir/$library.tar.xz && cd $baseDir/qt-everywhere-src-5.15.8
     fi
+
+    tar xvf $baseDir/$library.tar.xz && mkdir -p $baseDir/$library/build && cd !$
 
     CPPFLAGS=-I$installDir/include \
     LDFLAGS="-L$installDir/lib -Wl,-rpath=$installDir/lib" \
     CC=$CC \
     CXX=$CXX \
     ../configure \
-    -prefix $installDir \
+    -prefix=$installDir \
     -opensource \
     -confirm-license \
     -release \
